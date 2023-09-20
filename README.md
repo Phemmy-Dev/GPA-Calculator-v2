1dcad81a7e136ff96c89ec8a0b32c833835443d0
Oluwafemi <phemmy123x@gmail.com>
2023-09-20 02:09:05 +0100

    Added LocalStorage and action button

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
+
 # GPA Calculator
 
-This is a simple web-based GPA calculator that allows users to calculate their GPA for a semester and optionally save their results as an image. It also has a feature for light and dark mode.
+This is a web-based GPA calculator that allows users to calculate their GPA for a semester and optionally save their results as an image. It also features the ability to add and remove courses, a light and dark mode, and a history-clearing button.
 
 ## Table of Contents
 - [Features](#features)
 - [Demo](#demo)
 - [Screenshots](#screenshots)
+- [Usage](#usage)
+- [Clear History](#clear-history)
 - [Light and Dark Mode](#light-and-dark-mode)
 
 ## Features
@@ -14,22 +17,37 @@ This is a simple web-based GPA calculator that allows users to calculate their G
 - Add and remove courses from the calculation.
 - Option to download GPA results as an image for reference.
 - Toggle between light mode and dark mode for a personalized experience.
+- Save and load calculation history from browser storage.
+- Clear history to start fresh.
 
 ## Demo
 
-You can try the GPA Calculator here: [Demo](https://gpa-calculator1.netlify.app/) (Replace with your demo link if available)
+You can try the GPA Calculator here: [Demo Link](https://gpa-calculator1.netlify.app/)
 
 ## Screenshots
 
 ![GPA Calculator Screenshot](screenshot.png)
 
+## Usage
+
+1. Enter your course code, course unit, and grade.
+2. Click the "Add Grade" button to add the course to the calculation.
+3. Repeat the above steps for each course.
+4. Your GPA will be displayed in the "Total GPA" section.
+5. Optionally, click the "Download Screenshot" button to save your GPA results as an image.
+
+## Clear History
+
+To clear your calculation history:
+
+1. Click the "Clear History" button.
+2. This action will remove all saved calculation history from your browser.
 
 ## Light and Dark Mode
-Click the "Toggle Mode" button to switch between light mode and dark mode.
-In light mode, the background is light, and the text is dark.
-In dark mode, the background is dark, and the text is light.
 
-
+- Click the different modes button to switch between light mode and dark mode.
+- In light mode, the background is light, and the text is dark.
+- In dark mode, the background is dark, and the text is light.
 
 
 
--- main.js
+++ main.js
@@ -12,6 +12,8 @@ const gpaResults = document.getElementById("gpa-results");
 const toggleModeButton = document.getElementById("toggle-mode");
 const body = document.body;
 const gpaScoreSummary = document.getElementById("gpaSummary");
+const clearHistoryButton = document.getElementById("clear-history");
+
 
 const grades = [];
 
@@ -39,18 +41,58 @@ removeLastButton.addEventListener("click", () => {
   }
 });
 
+
+function removeCourseAtIndex(index) {
+  if (index >= 0 && index < grades.length) {
+    grades.splice(index, 1);
+    updateGradeTable();
+    calculateGPA();
+  }
+}
+
+// function to convert numeric grade points to letter grades
+function numericToLetterGrade(numericGrade) {
+  // mapping of numeric grades to letter grades here
+  if (numericGrade >= 4.5) {
+    return "A";
+  } else if (numericGrade >= 3.5) {
+    return "B";
+  } else if (numericGrade >= 2.5) {
+    return "C";
+  } else if (numericGrade >= 1.5) {
+    return "D";
+  } else if (numericGrade >= 0.5) {
+    return "E";
+  } else {
+    return "F";
+  }
+}
+
+
 function updateGradeTable() {
   gradeTable.innerHTML = "";
-  grades.forEach((entry) => {
+  grades.forEach((entry, index) => {
     const row = gradeTable.insertRow();
     const cell1 = row.insertCell(0);
     const cell2 = row.insertCell(1);
     const cell3 = row.insertCell(2);
+    const cell4 = row.insertCell(3);
     cell1.textContent = entry.courseCode;
     cell2.textContent = entry.courseUnit;
-    cell3.textContent = entry.grade;
+    
+    // Displaying the letter grade instead of numeric grade
+    cell3.textContent = numericToLetterGrade(entry.grade);
+
+    //A button to create a delete button and add an event listener
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.addEventListener("click", () => {
+      removeCourseAtIndex(index);
+    });
+    cell4.appendChild(deleteButton);
   });
 }
+
 
 function calculateGPA() {
   if (grades.length === 0) {
@@ -68,7 +110,28 @@ function calculateGPA() {
 
   const gpa = (totalPoints / totalUnits).toFixed(2);
   totalGpa.textContent = gpa;
+
+  // Saving the grades to localStorage
+  localStorage.setItem("grades", JSON.stringify(grades));
 }
+
+// Retrieving grades from localStorage on page load
+const savedGrades = JSON.parse(localStorage.getItem("grades"));
+if (savedGrades) {
+  grades.push(...savedGrades); // Push saved grades into the grades array
+  updateGradeTable();
+  calculateGPA();
+}
+
+
+
+clearHistoryButton.addEventListener("click", () => {
+  localStorage.removeItem("grades"); // Clear the saved grades from localStorage
+  grades.length = 0; // Clear the grades array
+  updateGradeTable();
+  calculateGPA();
+});
+
 
 function clearForm() {
   courseCodeInput.value = "";
@@ -77,13 +140,11 @@ function clearForm() {
 }
 
 downloadScreenshotButton.addEventListener("click", () => {
-  // Preload any custom fonts (if applicable)
-  // Example: document.fonts.load('16px YourCustomFont');
 
-  // Use html2canvas with configuration to capture at a higher scale
+
   html2canvas(gpaResults, {
     scale: 4, // Increase the scale factor for better quality
-    useCORS: true, // Enable CORS for external resources (e.g., images from other domains)
+    useCORS: true,
   }).then((canvas) => {
     // Create an "a" element to trigger the download
     const a = document.createElement("a");
@@ -93,7 +154,7 @@ downloadScreenshotButton.addEventListener("click", () => {
   });
 });
 
-// Toggle mode when the button is clicked
+// Toggle button when the button is clicked
 toggleModeButton.addEventListener("click", () => {
   body.classList.toggle("dark-mode");
 
@@ -106,3 +167,5 @@ toggleModeButton.addEventListener("click", () => {
     toggleModeButton.textContent = "Dark Mode"; // Switch to Dark mode
   }
 });
+
+
